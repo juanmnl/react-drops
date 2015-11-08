5fd4aa40680372a40de3dda5295d0ee86b4825c7
juanmnl <juanmnlcornejo@gmail.com>
2015-11-07 19:27:05 -0500

    Add shell.js and shell messages w/coloring

--- bin/droplet.js
+++ bin/droplet.js
@@ -1,9 +1,17 @@
 #! /usr/bin/env node
-console.log('Reactive liquid ready');
-setTimeout(function() {
-	console.log('Creating droplet...');
-}, 1000)
+var shell = require('shelljs');
+shell.exec("echo Hey there! How are you?");
 
 setTimeout(function() {
-	console.log('Droplet done!');
-}, 2000)
+	shell.exec('echo "\033[34mProcess: Scaning Project Structure \033[0m"');
+},1000)
+
+setTimeout(function() {
+	shell.exec('ls -l');
+	shell.exec('echo "\033[33mWarning: App folder Not Found!! \033[0m"')
+}, 4000)
+
+setTimeout(function() {
+	shell.exec('echo "\033[31mError: Component Not Created! \033[0m"')
+	shell.exec('echo "\033[32mSuccess: Component Created! \033[0m"')
+}, 5000)
