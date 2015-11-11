298c65be40cc203d1f41168569baf6083d30f576
juanmnl <juanmnlcornejo@gmail.com>
2015-11-10 22:22:30 -0500

    Add prompt for directories creation

--- bin/drops.js
+++ bin/drops.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 var shell = require('shelljs');
-var ask = require('inquirer');
+var inquirer = require('inquirer');
 
 
 ////* DROPS *////
 
-shell.echo('Preparing reactive fluid')
+shell.echo('Preparing reactive fluid');
 setTimeout(function() {
 	shell.echo('\033[34mProcess: Scaning Project for *needed* directories \033[0m');
 	shell.echo(process.cwd());
@@ -15,12 +15,30 @@ setTimeout(function() {
 	if (!shell.test('-e', 'app/components')) {
 		shell.echo('\033[33mSorry, Drops needs an "app/components" directory structure \033[0m');
 
-		// Inquirer for directory creation (yes/no)
+		// Create Directories?
+		var question = [
+			{
+		    type: "confirm",
+		    name: "createDirs",
+		    message: "Do you want to create them?",
+		    default: true
+		  }
+		]
+		inquirer.prompt(question, function(answer) {
+    	if (answer.createDirs !== false) {
+				shell.echo('Creating dirs...');
+				shell.exec('mkdir app && mkdir app/components');
+				shell.echo('\033[32mDirectories created \033[0m')
+				shell.exec('tree app/');
 
-		shell.exit(1);
+			} else {
+				shell.echo('Oh \033[34m:(\033[0m Bye, bye!');
+				shell.exit(1);
+			}
+		})
 	} else {
-		shell.echo('\033[32mSuccess: "app/components" Found! \033[0m');
 		shell.exec('ls -l'); // Highlight dir if possible
+		shell.echo('\033[32mSuccess: "app/components" Found! \033[0m');
 
 		// Create COMPONENT files
 		//// app/components/component.js
@@ -34,6 +52,9 @@ setTimeout(function() {
 
 
 ////* MESSAGE COLORING *////
+
+// Process
+// shell.echo('\033[34mProcess: {message} \033[0m');
 
 // Warning
 // shell.echo('\033[33mWarning: {message} \033[0m')
